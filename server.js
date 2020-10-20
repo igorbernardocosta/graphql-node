dac3e389356f3408b26c963594555f773a5874c3
Igor Bernardo Costa <igorbernardocosta@gmail.com>
2020-10-19 21:55:52 -0300

    Basic Types

--- server.js
+++ server.js
@@ -2,25 +2,34 @@ var express = require('express');
 var { graphqlHTTP } = require('express-graphql');
 var { buildSchema } = require('graphql');
 
-// Construct a schema, using GraphQL schema language
+// Construir um esquema, usando a linguagem de esquema GraphQL 
 var schema = buildSchema(`
-  type Query {
-    hello: String
+  tipo Query {
+    quoteOfTheDay: String
+    aleatório: Float!
+    rollThreeDice: [Int]
   }
 `);
 
-// The root provides a resolver function for each API endpoint
+// A raiz fornece uma função de resolução para cada endpoint da API 
 var root = {
-    hello: () => {
-        return 'Hello world!';
+    quoteOfTheDay: () = > {
+        return Math.aleatório() < 0,
+        5 ? 'Vá com calma' : 'A salvação está dentro';
+    },
+    aleatório: () = > {
+        retornar matemática.aleatório();
+    },
+    rollThreeDice: () = > {
+        retorno[1, 2, 3].mapa(_ = > 1 + Math.floor(Math.random() * 6));
     },
 };
 
 var app = express();
-app.use('/graphql', graphqlHTTP({
+app.use('/ graphql', graphqlHTTP({
     schema: schema,
     rootValue: root,
     graphiql: true,
 }));
-app.listen(4000);
-console.log('Running a GraphQL API server at http://localhost:4000/graphql');
+app.ouvir(4000);
+console.log('Executando um servidor GraphQL API em localhost: 4000 / graphql');
