3167b8c252a0bebf7b65dc9e12596f2937c3b930
Igor Bernardo Costa <igorbernardocosta@gmail.com>
2020-10-19 22:02:08 -0300

    Passing Arguments

--- server.js
+++ server.js
@@ -2,34 +2,29 @@ var express = require('express');
 var { graphqlHTTP } = require('express-graphql');
 var { buildSchema } = require('graphql');
 
-// Construir um esquema, usando a linguagem de esquema GraphQL 
+// Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
-  tipo Query {
-    quoteOfTheDay: String
-    aleatório: Float!
-    rollThreeDice: [Int]
+  type Query {
+    rollDice(numDice: Int!, numSides: Int): [Int]
   }
 `);
 
-// A raiz fornece uma função de resolução para cada endpoint da API 
+// The root provides a resolver function for each API endpoint
 var root = {
-    quoteOfTheDay: () = > {
-        return Math.aleatório() < 0,
-        5 ? 'Vá com calma' : 'A salvação está dentro';
-    },
-    aleatório: () = > {
-        retornar matemática.aleatório();
-    },
-    rollThreeDice: () = > {
-        retorno[1, 2, 3].mapa(_ = > 1 + Math.floor(Math.random() * 6));
-    },
+    rollDice: ({ numDice, numSides }) => {
+        var output = [];
+        for (var i = 0; i < numDice; i++) {
+            output.push(1 + Math.floor(Math.random() * (numSides || 6)));
+        }
+        return output;
+    }
 };
 
 var app = express();
-app.use('/ graphql', graphqlHTTP({
+app.use('/graphql', graphqlHTTP({
     schema: schema,
     rootValue: root,
     graphiql: true,
 }));
-app.ouvir(4000);
-console.log('Executando um servidor GraphQL API em localhost: 4000 / graphql');
+app.listen(4000);
+console.log('Running a GraphQL API server at localhost:4000/graphql');
